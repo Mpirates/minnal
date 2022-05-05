9dbec88c86dfb1742272ee6816717d1192c68945
Mrpirates <104631217+Mpirates@users.noreply.github.com>
2022-05-05 17:53:28 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/hackgit12/devil.git /EvaMaria
+  git clone https://github.com/Mpirates/minnal.git /EvaMaria
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /EvaMaria
