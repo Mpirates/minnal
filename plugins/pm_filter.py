d941c03a846b61c1435240401abe20a5a0a7e7f0
Mrpirates <104631217+Mpirates@users.noreply.github.com>
2022-05-05 18:41:57 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -130,7 +130,7 @@ async def next_page(bot, query):
     except MessageNotModified:
         pass
     await query.answer()
-    await asyncio.sleep(300)
+    await asyncio.sleep(600)
     await hehe.delete()
     await message.delete()
 
